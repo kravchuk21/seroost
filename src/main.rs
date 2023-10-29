8e581529cb4a9ddc4ae30b58a5d134a8bd3ba5d9
kravchuk21 <krauchukvlad@gmail.com>
2023-10-29 16:28:28 +0300

    feat: `read_entire_xml_file` function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io;
+use xml::reader::{EventReader, XmlEvent};
+
+fn read_entire_xml_file(file_path: &str) -> io::Result<String> {
+    let file = File::open(file_path)?;
+    let er = EventReader::new(file);
+    let mut content = String::new();
+
+    for event in er.into_iter() {
+        if let XmlEvent::Characters(text) = event.expect("TODO") {
+            content.push_str(&text);
+        }
+    }
+
+    Ok(content)
 }
+
+fn main() {
+    let file_path = "docs.gl/gl4/glClear.xhtml";
+    
+    println!("{content}", content = read_entire_xml_file(file_path).expect("TODO"));
+}
+ 
